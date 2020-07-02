58bcfa7dc77198ae19aa17303ac65a7e50c95122
yosef febrianes <yoseffebrianes@gmail.cm>
2020-07-02 07:18:52 +0700

    unRAR

---
+++ LeaveRequest/src/test/java/com/FinalProject/LeaveRequest/LeaveRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalProject.LeaveRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
